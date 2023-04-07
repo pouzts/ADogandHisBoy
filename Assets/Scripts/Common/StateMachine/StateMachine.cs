a658aea133384697b243b14921c8c79cdb1da6a0
Peter Ouzts <81652748+pouzts@users.noreply.github.com>
2023-04-06 22:11:04 -0600

    Completed State Machine Code

--- Assets/Scripts/Common/StateMachine/StateMachine.cs
+++ Assets/Scripts/Common/StateMachine/StateMachine.cs
@@ -4,17 +4,52 @@ using UnityEngine;
 
 public class StateMachine : MonoBehaviour
 {
-    public Dictionary<State, KeyValuePair<Transition, State>> stateTransitions;
     public State CurrentState { get; private set; }
+    private readonly Dictionary<State, List<KeyValuePair<Transition, State>>> stateTransitions = new();
 
-    void Update()
+    private void Update()
     {
         if (CurrentState != null)
             return;
 
         var transitions = stateTransitions[CurrentState];
 
+        foreach (var transition in transitions) 
+        { 
+            if (transition.Key.CheckConditions())
+            {
+                SetState(transition.Value);
+                break;
+            }
+        }
+
         CurrentState.OnUpdate();
+    }
+
+    public void AddState(State state) 
+    {
+        if (!stateTransitions.ContainsKey(state))
+        {
+            stateTransitions[state] = new List<KeyValuePair<Transition, State>>();
+        }
+    }
+
+    public void AddTransition(State stateFrom, Transition transition, State stateTo) 
+    {
+        if (stateTransitions.ContainsKey(stateFrom))
+        {
+            var transitions = stateTransitions[stateFrom];
+            transitions.Add(new KeyValuePair<Transition, State>(transition, stateTo));
+        }
+    }
+
+    public void AddTransition(string stateFrom, Transition transition, string stateTo)
+    {
+        if (stateTransitions.ContainsKey(GetState(stateFrom)))
+        {
+            var transitions = stateTransitions[GetState(stateFrom)];
+            transitions.Add(new KeyValuePair<Transition, State>(transition, GetState(stateTo)));
+        }
     }
 
     public void SetState(State state) 
@@ -29,6 +64,12 @@ public class StateMachine : MonoBehaviour
 
     public State GetState(string name) 
     {
+        foreach (var state in stateTransitions) 
+        {
+            if (string.Equals(state.Key.Name, name, System.StringComparison.OrdinalIgnoreCase))
+                return state.Key;
+        }
+
         return null;
     }
 
