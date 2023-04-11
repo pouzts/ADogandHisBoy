c005ad7484429f13f1efd985d40e004bcbe0f16f
Peter Ouzts <pouzts@student.neumont.edu>
2023-04-10 19:21:22 -0600

    Starting to work on the player.

--- Assets/Scripts/Common/Actors/Player.cs
+++ Assets/Scripts/Common/Actors/Player.cs
@@ -1,16 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private Transform cameraTransform;
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private float smoothTurnTime = 0.1f;
+
+    private CharacterController characterController;
+    private Vector3 direction = Vector3.zero;
+
+    private float turnVelcoity;
+
     private void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        
+        if (characterController == null || cameraTransform == null)
+            return;
+
+        if (direction.magnitude >= 0.1f) { 
+            float targetAngle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg + cameraTransform.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnVelcoity, smoothTurnTime);
+            transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
+
+            Vector3 dir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            characterController.Move(speed * Time.fixedDeltaTime * dir);
+        }
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Vector2 inputValue = context.ReadValue<Vector2>();
+        direction = new Vector3(inputValue.x, 0.0f, inputValue.y).normalized;
     }
 }
