c6263786caa4fdfb9dd8bc7bf99565e9f9ce00aa
Peter Ouzts <pouzts@student.neumont.edu>
2023-05-14 13:37:19 -0600

    Working on damping the player movement

--- Assets/Scripts/Actors/Dog/Player.cs
+++ Assets/Scripts/Actors/Dog/Player.cs
@@ -15,7 +15,6 @@ public class Player : MonoBehaviour
     [Header("Physics")]
     [SerializeField] private float gravity = -9.8f;
     [SerializeField] private float jumpHeight = 5f;
-    [SerializeField] private Vector3 drag = Vector3.zero;
 
     [Header("Ground Check")]
     [SerializeField] private Transform groundCheck;
@@ -27,14 +26,22 @@ public class Player : MonoBehaviour
 
     private Vector3 input = Vector3.zero;
     private Vector3 gravityVelocity = Vector3.zero;
+    private Vector3 dir = Vector3.zero;
+    private Vector3 velocity = Vector3.zero;
 
     private float turnVelcoity = 0f;
     private bool followPlayer = false;
 
     private bool isGrounded = false;
 
+    private float maxSpeed = 0f;
+    private float minSpeed = 0f;
+    private float speedTime = 0.5f;
+
+
     private void Start()
     {
+        maxSpeed = speed;
         controller = GetComponent<CharacterController>();
     }
 
@@ -48,15 +55,20 @@ public class Player : MonoBehaviour
         if (isGrounded)
             gravityVelocity.y = 0f;
 
-        if (input.magnitude >= 0.1f) { 
+        if (input.magnitude >= 0.1f)
+        {
             float targetAngle = Mathf.Atan2(input.x, input.z) * Mathf.Rad2Deg + cameraTransform.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnVelcoity, smoothTurnTime);
             transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
 
-            Vector3 dir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            Vector3 velocity = speed * dir.normalized;
-            velocity.x /= 1f + drag.x * Time.deltaTime;
-            velocity.z /= 1f + drag.z * Time.deltaTime;
+            dir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            velocity = Mathf.Lerp(minSpeed, maxSpeed, speedTime) * dir.normalized;
+            controller.Move(velocity * Time.deltaTime);
+        }
+        else
+        {
+            velocity = Mathf.Lerp(maxSpeed, minSpeed, speedTime) * dir.normalized;
+            if (velocity.magnitude <= 0.1f) return;
             controller.Move(velocity * Time.deltaTime);
         }
 
@@ -67,7 +79,10 @@ public class Player : MonoBehaviour
     public void OnMove(InputAction.CallbackContext context)
     {
         Vector2 inputValue = context.ReadValue<Vector2>();
-        input = new Vector3(inputValue.x, 0.0f, inputValue.y).normalized;
+        if (inputValue.magnitude >= 0.1f)
+        {
+            input = new Vector3(inputValue.x, 0.0f, inputValue.y).normalized;
+        }
     }
 
     public void OnFollowPlayer(InputAction.CallbackContext context) 
