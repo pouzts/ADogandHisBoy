434b546317ac58b2ac345868f9b9187e0146d55a
Peter Ouzts <81652748+pouzts@users.noreply.github.com>
2023-05-29 18:42:38 -0600

    Added to the player.cs

--- Assets/Scripts/Actors/Dog/Player.cs
+++ Assets/Scripts/Actors/Dog/Player.cs
@@ -5,62 +5,88 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [Header("References")]
     [SerializeField] private Transform cameraTransform;
+    //[SerializeField] private float smoothTurnTime = 0.1f;
+
+    [Header("Character Attributes")]
     [SerializeField] private float speed = 10f;
-    [SerializeField] private float smoothTurnTime = 0.1f;
-    
+
     [Header("Physics")]
-    [SerializeField] private float gravity = -9.8f;
+    //[SerializeField] private float gravity = -9.8f;
     [SerializeField] private float jumpHeight = 5f;
     [SerializeField] private float damping = 0.5f;
-    [SerializeField] private Vector3 velocity = Vector3.zero;
 
     [Header("Ground Check")]
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private float groundRadius = 0.25f;
 
-    private CharacterController controller;
-    //private Rigidbody rb;
+    //private CharacterController controller;
+    private Rigidbody rb;
 
+    private Vector3 velocity = Vector3.zero;
     private Vector3 input = Vector3.zero;
-
-    private float turnSpeed = 2f;
+    
     private bool isGrounded = false;
 
+    private float turnSpeed = 0f;
+    private float smoothTurnTime = 0.05f;
 
     private void Start()
     {
-        controller = GetComponent<CharacterController>();
+        //controller = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
     }
 
-    private void Update()
-    {   
-        if (controller == null || cameraTransform == null)
-            return;
-        
+    private void FixedUpdate()
+    {
+        //isGrounded = controller.isGrounded;
+
+        //if (controller == null || cameraTransform == null)
+        //return;
+
         isGrounded = Physics.CheckSphere(groundCheck.position, groundRadius, groundLayer);
 
-        if (isGrounded)
-            velocity.y = 0f;
+        if (rb == null || cameraTransform == null)
+            return;
 
         if (input.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(input.x, input.z) * Mathf.Rad2Deg + cameraTransform.eulerAngles.y;
-            //float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSpeed, smoothTurnTime);
-            transform.rotation = Quaternion.Euler(0.0f, targetAngle, 0.0f);
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSpeed, smoothTurnTime);
+            transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
 
-            Vector3 dir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            Vector3 dir = Quaternion.Euler(0.0f, targetAngle, 0.0f) * Vector3.forward;
             velocity = speed * dir.normalized;
         }
         else
         {
-            velocity.x *= 1f / (1f + (damping * Time.deltaTime));
-            velocity.z *= 1f / (1f + (damping * Time.deltaTime));
+            velocity.x *= 1.0f / (1.0f + (damping * Time.fixedDeltaTime));
+            velocity.z *= 1.0f / (1.0f + (damping * Time.fixedDeltaTime));
         }
 
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
+        //if (!isGrounded)
+        //    velocity.y += gravity * Time.fixedDeltaTime;
+        
+        if (isGrounded)
+            rb.useGravity = false;
+        else
+            rb.useGravity = true;
+
+        //controller.Move(velocity * Time.deltaTime);
+        rb.position += velocity * Time.fixedDeltaTime;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (isGrounded)
+            Gizmos.color = Color.red;
+        else
+            Gizmos.color = Color.green;
+
+        Gizmos.DrawSphere(groundCheck.position, groundRadius);
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -87,10 +113,10 @@ public class Player : MonoBehaviour
 
     public void OnJump(InputAction.CallbackContext context) 
     {
-        if (context.performed && isGrounded) 
-        {
-            float jump = Mathf.Sqrt(jumpHeight * -2 * gravity);
-            velocity.y += jump;
+        if (context.started && isGrounded)
+        { 
+            float jump = Mathf.Sqrt(jumpHeight * -2 * Physics.gravity.y * rb.mass);
+            rb.AddForce(jump * Time.fixedDeltaTime * Vector3.up, ForceMode.Impulse);
         }
     }
 }
